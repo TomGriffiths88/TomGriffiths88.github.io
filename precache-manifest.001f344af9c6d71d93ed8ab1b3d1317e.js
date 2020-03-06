22a3ff93d17f92f1681aac8675a391974b176304
Tom Griffiths <tom@tomgriffithsphotos.com>
2020-03-06 13:36:05 +0000

    Updates

---
+++ precache-manifest.001f344af9c6d71d93ed8ab1b3d1317e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26c8d526345e3df6b04b3d1d1db0fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62a59b63351d40e348a",
+    "url": "/static/css/main.30646290.chunk.css"
+  },
+  {
+    "revision": "b87e9cc06823b3bf0847",
+    "url": "/static/js/2.1937bd33.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1937bd33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62a59b63351d40e348a",
+    "url": "/static/js/main.85f97c02.chunk.js"
+  },
+  {
+    "revision": "bd2bc38ea6b33666ab3f",
+    "url": "/static/js/runtime-main.95d8fd95.js"
+  },
+  {
+    "revision": "e5a41455757edcde946174423404e4a9",
+    "url": "/static/media/angelina.e5a41455.jpg"
+  },
+  {
+    "revision": "a2c0fa511a82bb680e7cdc2ac4f09b58",
+    "url": "/static/media/cocktail.a2c0fa51.jpg"
+  },
+  {
+    "revision": "7c0605a844a63db8bb42aae8d749b58a",
+    "url": "/static/media/crochet.7c0605a8.jpg"
+  },
+  {
+    "revision": "0ddebf135a4315cdf5e16ecee8f2cfae",
+    "url": "/static/media/photo.0ddebf13.jpg"
+  },
+  {
+    "revision": "5f3284f06e8c3ca25d9b25072363068e",
+    "url": "/static/media/weather.5f3284f0.jpg"
+  },
+  {
+    "revision": "7b775c33a681ee0ad8e748ff097996b6",
+    "url": "/static/media/wildrose.7b775c33.jpg"
+  }
+]);
